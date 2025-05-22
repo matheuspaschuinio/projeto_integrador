1ebb7559ed80b6cb8b22156be9356086252cc0e4
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-05-22 11:59:45 -0300

    Adicionando validação básica do login com js

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+/** Variáveis */
+const inputLogin = document.getElementById("input-login");
+const inputSenha = document.getElementById("input-senha");
+const btnEntrar = document.getElementById("btn-entrar");
+
+function entrar() {
+  if (inputLogin.value === "aluno1" && inputSenha.value === "aluno1") {
+    alert("Bem vindo aluno1");
+  } else if (inputLogin.value == "prof1" && inputSenha.value == "prof1") {
+    alert("Bem vindo prof1");
+  } else if (inputLogin.value == "direcao1" && inputSenha.value == "direcao1") {
+    alert("Bem vindo direcao1");
+  } else if (inputLogin.value == "pais1" && inputSenha.value == "pais1") {
+    alert("Bem vindo pais1");
+  } else if (inputLogin.value == "" && inputSenha.value == "") {
+    alert("Preencha os campos login e senha!");
+  } else {
+    alert("login ou senha inválidos!");
+  }
+}
+
+/**
+
+btnEntrar.addEventListener("click", function () {
+  alert("Bem vindo aluno1");
+  if (inputLogin === "aluno1" && inputSenha === "aluno1") {
+    alert("Bem vindo aluno1");
+  }
+
+  //   if (inputLogin == "prof1" && inputSenha == "prof1") {
+  //     alert("Bem vindo prof1");
+  //   }
+
+  //   if (inputLogin == "direcao1" && inputSenha == "direcao1") {
+  //     alert("Bem vindo direcao1");
+  //   }
+
+  //   if (inputLogin == "pais1" && inputSenha == "pais1") {
+  //     alert("Bem vindo pais1");
+  //   }
+});
+
+*/
